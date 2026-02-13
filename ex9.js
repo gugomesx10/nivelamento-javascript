419ee336dc75de92053e8b24f6915648362005c0
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-02-12 22:20:44 -0300

    2.7 - Verificador de notas

---
+++ ex9.js
@@ -0,0 +1,29 @@
+let nota = 8;
+
+if (nota >= 7) {
+  console.log("Aprovado");
+} else if (nota >= 5) {
+  console.log("Recuperação");
+} else {
+  console.log("Reprovado");
+}
+
+nota = 6;
+
+if (nota >= 7) {
+  console.log("Aprovado");
+} else if (nota >= 5) {
+  console.log("Recuperação");
+} else {
+  console.log("Reprovado");
+}
+
+nota = 3;
+
+if (nota >= 7) {
+  console.log("Aprovado");
+} else if (nota >= 5) {
+  console.log("Recuperação");
+} else {
+  console.log("Reprovado");
+}
