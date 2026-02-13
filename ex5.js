47e7327af36c39427075803f0ac3f387d704a741
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-02-12 22:12:37 -0300

    Simulei um sistema de delivery

---
+++ ex5.js
@@ -0,0 +1,11 @@
+let enderecoEntrega = null;
+let numeroPedido;
+
+console.log("Endereço:", enderecoEntrega);
+console.log("Número do Pedido:", numeroPedido);
+
+enderecoEntrega = "Rua das Flores, 123";
+numeroPedido = 456789;
+
+console.log("Endereço:", enderecoEntrega);
+console.log("Número do Pedido:", numeroPedido);
