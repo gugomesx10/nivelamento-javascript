23fc0dd041e1a7119f823252f50419665d4313e5
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-02-12 22:14:11 -0300

    2.4 - Criei o aluno e anota com notas citadas

---
+++ ex6.js
@@ -0,0 +1,10 @@
+let aluno = { nome: "Carlos", matricula: 2024001 };
+
+console.log("Nome:", aluno.nome);
+console.log("Matrícula:", aluno.matricula);
+
+console.log("Nota:", aluno.nota);
+
+aluno.nota = 8.5;
+
+console.log("Nota:", aluno.nota);
