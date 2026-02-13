a0cca9855b026f2a7f50f697a1341047aa2fc2f1
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-02-12 22:25:39 -0300

    2.9 - Utilizei o for para resolver esta questão utilizando números pares e percorrendo com array de nomes familiares

---
+++ ex11.js
@@ -0,0 +1,9 @@
+for (let i = 0; i <= 10; i += 2) {
+  console.log(i);
+}
+
+let alunos = ["Gustavo", "Giselen", "Cecília", "Omar"];
+
+for (let i = 0; i < alunos.length; i++) {
+  console.log(i + ": " + alunos[i]);
+}
