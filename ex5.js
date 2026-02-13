98a78914a30f5b795507c17da306e5764812e51a
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-02-12 22:27:55 -0300

    3.0 - simulei a contagem

--- ex5.js
+++ ex5.js
@@ -4,7 +4,7 @@ let numeroPedido;
 console.log("Endereço:", enderecoEntrega);
 console.log("Número do Pedido:", numeroPedido);
 
-enderecoEntrega = "Rua das Flores, 123";
+enderecoEntrega = "rua aleatorio, 123";
 numeroPedido = 456789;
 
 console.log("Endereço:", enderecoEntrega);
