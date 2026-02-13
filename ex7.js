7fdc676ec98af7850bac979c788642fbdb660166
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-02-12 22:15:27 -0300

    2.5 - Criei as arrays com 3 tarefas do dia

---
+++ ex7.js
@@ -0,0 +1,10 @@
+let tarefas = ["Estudar JavaScript", "Fazer exercícios", "Revisar código"];
+
+console.log("Primeira tarefa:", tarefas[0]);
+console.log("Última tarefa:", tarefas[2]);
+
+console.log("Total de tarefas:", tarefas.length);
+
+tarefas.push("Fazer commit no Git");
+
+console.log("Novo total de tarefas:", tarefas.length);
