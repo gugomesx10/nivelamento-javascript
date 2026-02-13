63eaa20e3a840e3b14e27a8184b7cbf00b54b15d
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-02-12 22:09:18 -0300

    2.1 - Feito a simulação de placar de jogo

---
+++ ex3.js
@@ -0,0 +1,10 @@
+const nomeJogador = 'Gustavo';
+
+let pontuacao = 0;
+
+console.log('Jogador:', nomeJogador);
+console.log('Pontuação inicial:', pontuacao);
+
+pontuacao = 10;
+
+console.log('Pontuação atualizada:', pontuacao);
