681d1c098c135e93b71f73a0893b30e15ca1c48d
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-02-12 22:17:57 -0300

    2.6 - Criei chamados de veiculos com propriedades

---
+++ ex8.js
@@ -0,0 +1,15 @@
+let veiculo = {
+  marca: "Toyota - Civic",
+  modelo: "Subaru",
+  ano: 2023,
+  disponivel: true
+};
+
+console.log("Marca:", veiculo.marca);
+console.log("Modelo:", veiculo.modelo);
+
+console.log("Ano:", veiculo["ano"]);
+console.log("Disponível:", veiculo["disponivel"]);
+
+veiculo.cor = "Prata";
+console.log("Cor:", veiculo.cor);
