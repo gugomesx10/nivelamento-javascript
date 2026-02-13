8c4ab3ed17339e14b2817534d4c4729f1a68bcec
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-02-12 22:10:47 -0300

    2.2 - Criado 4 variáveis com tipos diferentes

---
+++ ex4.js
@@ -0,0 +1,12 @@
+let nome = "Gustavo";
+
+let idade = 25;
+
+let ativo = true;
+
+let semValor;
+
+console.log("Nome:", nome, "| Tipo:", typeof nome);
+console.log("Idade:", idade, "| Tipo:", typeof idade);
+console.log("Ativo:", ativo, "| Tipo:", typeof ativo);
+console.log("Sem Valor:", semValor, "| Tipo:", typeof semValor);
